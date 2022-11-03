fb54c1a77d75ad8810b8fb6dab9c86f9fc217ac0
Волков Егор <egor.volkov20038@gmail.com>
2022-11-03 03:43:32 +0300

    added_heat_map_finally

--- web/main.js
+++ web/main.js
@@ -1,7 +1,7 @@
 import {add_heat_map} from "./districts/add_heat_map.js";
 import {get_polylabel_arr} from "./districts/get_polylabel_arr.js";
 import {get_areas_arr} from "./areas/get_areas_arr.js";
-import {add_heat_map_postamats1} from "./districts/test.js";
+import {get_postamats_data} from "./districts/get_postamats_data.js";
 
 ymaps.ready(['polylabel.create']).then(function () {
     const myMap = new ymaps.Map("map", {
@@ -18,6 +18,7 @@ ymaps.ready(['polylabel.create']).then(function () {
             objectManager.add(item);
     })
     const areas_arr = get_areas_arr();
+    const data1 = get_postamats_data()
 
     function add_areas() {
         areas_arr.forEach(function (item, i, district_arr) {
@@ -57,13 +58,28 @@ ymaps.ready(['polylabel.create']).then(function () {
             remove_areas();
         }
     }
+    ymaps.modules.require(['Heatmap'], function (Heatmap) {     // Тепловая карта
+        var data = data1,
+        heatmap = new Heatmap(data);
+        document.getElementById("heat_map_postamats").onchange = function () {
+            if(document.getElementById("heat_map_postamats").checked) {
+                heatmap.setMap(myMap)
+            }
+            else {
+                heatmap.destroy()
+            }
+        }
+    });
+
+
+
 
     myMap.events.add('boundschange', function (event) {
     if (event.get('newZoom') !== event.get('oldZoom')) {    // Ловим изменение "зума" карты
         //alert('Уровень масштабирования изменился.');
 
     }
-    add_heat_map_postamats1(myMap);
+    get_postamats_data(myMap);
 
 });})
 
